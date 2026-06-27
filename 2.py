00a4574c14fcbd9ed1536037308eb3751dea5d3e
kuznetsovroman41 <romank04052006@gmail.com>
2026-06-27 12:57:11 +0600

    добавил ограничения

--- 2.py
+++ 2.py
@@ -1,12 +1,30 @@
 from math import log, tan, sin
+
 a = int(input("Введите первое число: "))
-b = int(input("Введите втрое число: "))
+while a == 0:
+    a = int(input("Ошибка! Введите a ≠ 0: "))
+
+b = int(input("Введите второе число: "))
+while b == 0:
+    b = int(input("Ошибка! Введите b ≠ 0: "))
+
 y = int(input("Введите третье число: "))
+while y == 0:
+    y = int(input("Ошибка! Введите y ≠ 0: "))
+
 x = int(input("Введите четвертое число: "))
+while x == 0:
+    x = int(input("Ошибка! Введите x ≠ 0: "))
 
-t1 = (1 / b**2) * (log(a / x) + (a * x) / y)
+t1 = (1 / b ** 2) * (log(a / x) + (a * x) / y)
 
-t2 = (x / a) * tan((a * x) / 2) + (2 / a**2) * log(sin((a * x) / 2))
+sin_val = sin((a * x) / 2)
 
-print(t1)
-print(t2)
+if sin_val > 0:
+    t2 = (x / a) * tan((a * x) / 2) + (2 / a ** 2) * log(sin_val)
+else:
+    t2 = None
+    print("Ошибка: логарифм не существует")
+
+print("t1 =", t1)
+print("t2 =", t2)
